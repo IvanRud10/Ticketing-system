7e67664d06ec0954bd511b066f5af3093ddf64a8
Ivan <72566481+IvanRud10@users.noreply.github.com>
2021-01-10 13:07:25 +0200

    Update Client1.cs

--- AirTicket/Client1.cs
+++ AirTicket/Client1.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
-//using System.ComponentModel.DataAnnotations;
-//using System.ComponentModel.DataAnnotations.Schema;
 
-//namespace AirTicket.Model
 namespace AirTicket
 {
     public class Client
